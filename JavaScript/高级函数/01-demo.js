a9bbce1c7ba867a0307a7cc34844180db04108d6
shmao <shmao@iflytek.com>
2020-12-14 15:27:18 +0800

    Update 高阶函数

--- JavaScript/高级函数/01-demo.js
+++ JavaScript/高级函数/01-demo.js
@@ -2,7 +2,7 @@
  * @Author: shmao
  * @Date: 2020-12-08 14:46:15
  * @LastEditors: shmao
- * @LastEditTime: 2020-12-08 14:57:28
+ * @LastEditTime: 2020-12-14 09:34:10
  */
 const Types = ["String", "Boolean", "Object", "Function", "Null", "Undefined", "Array", "Date", "Number"];
 
@@ -12,12 +12,20 @@ const Types = ["String", "Boolean", "Object", "Function", "Null", "Undefined", "
 
 // console.log(_isType(2, "Number"));
 
+// 柯里化
 function isType(type) {
   return function (value) {
     return Object.prototype.toString.call(value).includes(type);
   };
 }
 
+const fns = {};
+Types.forEach(type=>{
+  fns[`is${type}`] = isType(type);
+})
+
 const fn = isType("Number");
 
 console.log(fn(2));
+
+console.log(fns.isNumber(2));
