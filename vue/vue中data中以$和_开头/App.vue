66fba4b0e02e444a57cf15a839e8e88f68321273
shmao <shmao@iflytek.com>
2020-12-14 15:27:02 +0800

    Create vue中以$和_开头的变量

---
+++ vue/vue中data中以$和_开头/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>
+      {{ $data.$d }}
+    </div>
+    <div>
+      {{ list }}
+    </div>
+    <!-- {{ _data._i }} -->
+  </div>
+</template>
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      list: [1, 2, { a: 1 }],
+      $d: {}, // Property "$d" must be accessed with "$data.$d" because properties starting with "$" or "_" are not proxied in the Vue instance to prevent conflicts with Vue internals.
+      //   _i: 'fsdf', //https://segmentfault.com/a/1190000016885433
+    };
+  },
+  created() {
+    document.addEventListener('click', () => {
+      //   this.list = [2, 3];
+      this.list = {};
+      //   alert(this._data._i)
+    });
+    document.addEventListener('dblclick', () => {
+      this.list = [1, 2];
+      this.list.push(1);
+    });
+  },
+};
+</script>
