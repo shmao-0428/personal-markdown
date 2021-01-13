b989587230884f680ff2aa9317cca5a5b94232a5
shmao <maosuhui@gmail.com>
2021-01-13 20:04:15 +0800

    Update 树形结构数据的处理

--- JavaScript/JavaScriptUtilsLibray/tree.js
+++ JavaScript/JavaScriptUtilsLibray/tree.js
@@ -15,13 +15,13 @@
 const nest = (items, id = null, link = 'parent_id') =>
   items.filter((item) => item[link] === id).map((item) => ({ ...item, children: nest(items, item.id) }));
 
-// const _nest = (items, id = null, link = 'parent_id') => {
-//   let filters = items.filter((item) => item[link] === id);
-//   console.log('filter>>>', filters, 'id>>>', id);
-//   let res = filters.map((item) => ({ ...item, children: _nest(items, item.id) }));
-//   console.log('res>>>', res);
-//   return res;
-// };
+const _nest = (items, id = null, link = 'parent_id') => {
+  let filters = items.filter((item) => item[link] === id);
+  // console.log('filter>>>', filters, 'id>>>', id);
+  let res = filters.map((item) => ({ ...item, children: _nest(items, item.id) }));
+  // console.log('res>>>', res);
+  return res;
+};
 
 /** 用法 */
 const comments = [
@@ -31,11 +31,26 @@ const comments = [
   { id: 4, parent_id: 2 },
   { id: 5, parent_id: 4 },
 ];
+
 console.time();
-const nestedComments = nest(comments); // [{ id: 1, parent_id: null, children: [...] }]
+const nestedComments = _nest(_comments); // [{ id: 1, parent_id: null, children: [...] }]
 // console.log(nestedComments);
 console.log(JSON.stringify(nestedComments));
 console.timeEnd();
+
+const _comments = [
+  { id: 1, parent_id: null },
+  { id: 2, parent_id: 1 },
+  { id: 3, parent_id: 1 },
+  { id: 4, parent_id: 2 },
+  { id: 5, parent_id: 4 },
+  { id: 6, parent_id: null },
+  { id: 7, parent_id: 6 },
+  { id: 8, parent_id: 6 },
+  { id: 9, parent_id: 7 },
+  { id: 10, parent_id: 8 },
+  { id: 11, parent_id: 9 },
+];
 
 /**
  * 推荐: time usage for function nest >>> treeData
@@ -54,15 +69,27 @@ function treeData(source, id = 'id', parentId = 'parent_id', children = 'childre
   });
 }
 console.time();
-const tree = treeData(comments);
+const tree = treeData(_comments);
 console.log(JSON.stringify(tree));
 console.timeEnd();
 
 /** 树形结构扁平化 https://blog.csdn.net/Mr_JavaScript/article/details/102833991 */
 let res = [];
-const treeToFlat = (source) => {
+const fn = (source) => {
   source.forEach((el) => {
     res.push(el);
     el.children && el.children.length > 0 ? fn(el.children) : '';
   });
 };
+fn([
+  {
+    id: 1,
+    parent_id: null,
+    children: [
+      { id: 2, parent_id: 1, children: [{ id: 4, parent_id: 2, children: [{ id: 5, parent_id: 4, children: [] }] }] },
+      { id: 3, parent_id: 1, children: [] },
+    ],
+  },
+]);
+console.log(JSON.stringify(res));
+console.log(res);
