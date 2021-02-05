51f772d2f391d45cbb6cfc871813735cd2c84818
shmao <shmao@iflytek.com>
2021-02-05 14:18:36 +0800

    feat: js继承为什么要把子类的__proto__指向父类

--- JavaScript/面向对象的三大特性/继承/06-寄生式组合式继承.js
+++ JavaScript/面向对象的三大特性/继承/06-寄生式组合式继承.js
@@ -1,11 +1,17 @@
-function inheritPrototype(son, father) {
-  let prototype = Object.create(father.prototype);
-  prototype.constructor = son;
-  son.prototype = prototype;
+function inheritPrototype(Son, Father) {
+  let prototype = Object.create(Father.prototype);
+  prototype.constructor = Son;
+  Son.prototype = prototype;
+  // Son.__proto__ = Father;
+  // Object.setPrototypeOf(Son, Father);
 }
 
 function Father(name) {
   this.name = name;
+}
+
+Father.getCode = function() {
+  console.log('this is father code');
 }
 
 Father.prototype.getName = function () {
@@ -25,9 +31,21 @@ let father = new Father('father');
 
 let son = new Son('son');
 
-console.log(father);
-console.log(son);
-son.getAge();
-son.getName();
-console.log(Father.prototype.constructor);
-console.log(Son.prototype.constructor);
+// console.log(father);
+// console.log(son);
+// son.getAge();
+// son.getName();
+// console.log(Father.prototype.constructor);
+// console.log(Son.prototype.constructor);
+
+
+// js 继承为什么要把子类的__proto__指向父类
+// console.dir(Son.__proto__ === Function.prototype); // true
+// console.log(Son.__proto__.__proto__ === Object.prototype); // true
+
+// console.dir(Son.__proto__ === Father); // true
+// console.log(Son.__proto__.__proto__ === Function.prototype); // true
+// console.log(Son.__proto__.__proto__.__proto__ === Object.prototype); // true
+
+// 为了继承父类的静态方法
+Son.getCode()
